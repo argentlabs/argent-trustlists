6c75f248ada03009fbd7f8b97e1cccba76cd27f2
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2021-11-05 14:05:07 +0100

    Test environment variables

---
+++ scripts/debug.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+const ethers = hre.ethers;
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+  console.log(`signer is ${signer.address}`);
+  console.log(`ALCHEMY_KEY is ${process.env.ALCHEMY_KEY}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
