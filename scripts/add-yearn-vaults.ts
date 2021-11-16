7d418a83a53e4414a9064e150546573a81de861b
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2021-11-16 23:33:16 +0100

    Revert "Update add-yearn-vaults.ts"
    This reverts commit bcc3dc35445e025d33a58905f346340465ff0942.

--- scripts/add-yearn-vaults.ts
+++ scripts/add-yearn-vaults.ts
@@ -6,53 +6,55 @@ import { MultisigExecutor } from "./utils/multisigexecutor";
 
 const TRUSTLIST = 0;
 
-(async () => {
-  try {
-    const configLoader = new ConfigLoader(hre.network.name);
-    const config = configLoader.load();
-    const configUpdate = clonedeep(config);
+async function main() {
+  const configLoader = new ConfigLoader(hre.network.name);
+  const config = configLoader.load();
+  const configUpdate = clonedeep(config);
 
-    const [deployer] = await ethers.getSigners();
-    console.log(`Deployer is ${deployer.address} with balance ETH ${ethers.utils.formatEther(await ethers.provider.getBalance(deployer.address))}`);
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deployer is ${deployer.address} with balance ETH ${ethers.utils.formatEther(await ethers.provider.getBalance(deployer.address))}`);
 
-    const dappRegistry = await ethers.getContractAt("DappRegistry", config.dappRegistry.address);
-    const yearnV2Filter = await ethers.getContractAt("YearnV2Filter", config.yearn.v2.filter);
-    const registryOwner = await dappRegistry.registryOwners(TRUSTLIST);
-    console.log(`dapp registry is ${dappRegistry.address}`);
-    console.log(`yearn v2 filter is ${yearnV2Filter.address}`);
-    console.log(`registry owner is ${registryOwner}`);
+  const dappRegistry = await ethers.getContractAt("DappRegistry", config.dappRegistry.address);
+  const yearnV2Filter = await ethers.getContractAt("YearnV2Filter", config.yearn.v2.filter);
+  const registryOwner = await dappRegistry.registryOwners(TRUSTLIST);
+  console.log(`dapp registry is ${dappRegistry.address}`);
+  console.log(`yearn v2 filter is ${yearnV2Filter.address}`);
+  console.log(`registry owner is ${registryOwner}`);
 
-    if (config.argent.multisig.address.toLowerCase() !== registryOwner.toLowerCase()) {
-      console.error("Registry owner should be multisig");
-      return;
-    }
+  if (config.argent.multisig.address.toLowerCase() !== registryOwner.toLowerCase()) {
+    console.error("Registry owner should be multisig");
+    return;
+  }
 
-    const multisigExecutor = new MultisigExecutor(config.argent.multisig.autosign);
-    await multisigExecutor.connect(config.argent.multisig.address);
-    console.log("multisig executor connected");
+  const multisigExecutor = new MultisigExecutor(config.argent.multisig.autosign);
+  await multisigExecutor.connect(config.argent.multisig.address);
+  console.log("multisig executor connected");
 
-    const yvYFI = "0xdb25cA703181E7484a155DD612b06f57E12Be5F0";
-    const yvUSDC = "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE";
+  const yvYFI = "0xdb25cA703181E7484a155DD612b06f57E12Be5F0";
+  const yvUSDC = "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE";
 
-    let vault, transaction;
+  let vault, transaction;
 
-    vault = yvYFI;
-    console.log("adding yvYFI vault");
-    transaction = await multisigExecutor.executeCall(dappRegistry, "addDapp", [TRUSTLIST, vault, yearnV2Filter.address]);
-    console.log(`hash ${transaction.hash}`);
-    console.log(`Added new YearnV2 vault ${vault} for filter ${yearnV2Filter.address}`);
-    configUpdate.yearn.v2.vaults.push(vault);
-    configLoader.save(configUpdate);
+  vault = yvYFI;
+  console.log("adding yvYFI vault");
+  transaction = await multisigExecutor.executeCall(dappRegistry, "addDapp", [TRUSTLIST, vault, yearnV2Filter.address]);
+  console.log(`hash ${transaction.hash}`);
+  console.log(`Added new YearnV2 vault ${vault} for filter ${yearnV2Filter.address}`);
+  configUpdate.yearn.v2.vaults.push(vault);
+  configLoader.save(configUpdate);
 
-    vault = yvUSDC;
-    console.log("adding yvUSDC vault");
-    transaction = await multisigExecutor.executeCall(dappRegistry, "addDapp", [TRUSTLIST, vault, yearnV2Filter.address]);
-    console.log(`hash ${transaction.hash}`);
-    console.log(`Added new YearnV2 vault ${vault} for filter ${yearnV2Filter.address}`);
-    configUpdate.yearn.v2.vaults.push(vault);
-    configLoader.save(configUpdate);
-  } catch (error) {
+  vault = yvUSDC;
+  console.log("adding yvUSDC vault");
+  transaction = await multisigExecutor.executeCall(dappRegistry, "addDapp", [TRUSTLIST, vault, yearnV2Filter.address]);
+  console.log(`hash ${transaction.hash}`);
+  console.log(`Added new YearnV2 vault ${vault} for filter ${yearnV2Filter.address}`);
+  configUpdate.yearn.v2.vaults.push(vault);
+  configLoader.save(configUpdate);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
     process.exit(1);
-  }
-})();
+  });
