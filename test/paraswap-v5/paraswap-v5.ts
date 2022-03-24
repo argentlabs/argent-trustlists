1f6b05e557f8e006d35826547e7cead52e543a9c
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-03-24 18:31:53 +0000

    test(paraswap): update tests with simpler approach

--- test/paraswap-v5/paraswap-v5.ts
+++ test/paraswap-v5/paraswap-v5.ts
@@ -1,89 +1,53 @@
-import { TransactionRequest } from "@ethersproject/abstract-provider";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { BigNumber, Contract, ContractFactory } from "ethers";
+import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
-import { getTransaction, signCalldata } from "./paraswap.service";
+import { paraswapInterface, signCalldata } from "./paraswap.service";
 
-const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
-const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const AUGUSTUS_ADDRESS = "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57";
-
 const REGISTRY_ID = 0;
 const TIMELOCK = 0;
 
-const ethAmount = ethers.utils.parseEther("0.1").toString();
-const daiAmount = ethers.utils.parseEther("100").toString();
-
 describe("Paraswap", function () {
   let DappRegistry: ContractFactory;
-  let AugustusSwapperForwarder: ContractFactory;
   let ParaswapFilter: ContractFactory;
 
   let registry: Contract;
   let deployer: SignerWithAddress;
   let wallet: SignerWithAddress;
   let ags: SignerWithAddress;
-  let ethToDaiTransaction: TransactionRequest;
 
   before(async function () {
     DappRegistry = await ethers.getContractFactory("DappRegistry");
-    AugustusSwapperForwarder = await ethers.getContractFactory("AugustusSwapperForwarder");
     ParaswapFilter = await ethers.getContractFactory("ParaswapV5Filter");
     registry = await DappRegistry.deploy(TIMELOCK);
     [deployer, wallet, ags] = await ethers.getSigners();
   });
 
   describe("Testing filter for Paraswap v5", function () {
-    let augustus: any;
-    let forwarder: any;
-    let filter: any;
-    let dai: any;
+    let filter: Contract;
 
     before(async function () {
-      const paraswapInterface = "contracts/filters/paraswap-v5/swapper-contracts/IParaswap.sol:IParaswap";
-      augustus = await ethers.getContractAt(paraswapInterface, AUGUSTUS_ADDRESS);
-
-      forwarder = await AugustusSwapperForwarder.deploy(ethers.constants.AddressZero, augustus.address);
-      let role = "0x7a05a596cb0ce7fdea8a1e1ec73be300bdb35097c944ce1897202f7a13122eb2"
-      const simpleSwap = await ethers.getContractAt("SimpleSwap", "0xa655d02670be0ceC6b8b6b83c68Ed5375a2f5028");
-      let tx = await forwarder.grantRole(role, simpleSwap.address);
-      await tx.wait();
-      tx = await forwarder.setImplementation(simpleSwap.interface.getSighash("simpleSwap"), simpleSwap.address);
-      await tx.wait()
-
-      filter = await ParaswapFilter.deploy(ags.address);
-      await registry.addDapp(REGISTRY_ID, forwarder.address, filter.address);
-
-      dai = await ethers.getContractAt("IERC20", DAI_ADDRESS);
-      ethToDaiTransaction = await getTransaction(ETH_ADDRESS, dai.address, ethAmount, deployer.address);
+      filter = await ParaswapFilter.deploy(ags.address, AUGUSTUS_ADDRESS);
+      await registry.addDapp(REGISTRY_ID, AUGUSTUS_ADDRESS, filter.address);
     });
 
-    it("Should accept swapOnUniswap", async function () {
-      const amountIn = ethAmount;
+    it("Should accept swap data signed by the ags", async function () {
+      const amountIn = ethers.utils.parseEther("0.1");
       const amountOutMin = "1";
       const path = [ethers.constants.AddressZero];
-      const swapData = augustus.interface.encodeFunctionData("swapOnUniswap", [amountIn, amountOutMin, path]);
-      const signature = await signCalldata(swapData, ags);
+      const swapData = paraswapInterface.encodeFunctionData("swapOnUniswap", [amountIn, amountOutMin, path]);
 
-      const data = forwarder.interface.encodeFunctionData("delegateToParaswap", [signature, swapData]);
-      const isAuthorised = await registry.isAuthorised(wallet.address, forwarder.address, forwarder.address, data);
+      const latestBlock = await ethers.provider.getBlock("latest");
+      const deadline = ethers.utils.hexZeroPad(ethers.utils.hexlify(latestBlock.timestamp + 60), 32);
+
+      const dataToSign = ethers.utils.concat([wallet.address, deadline, swapData]);
+      const signature = await signCalldata(dataToSign, ags);
+
+      const data = ethers.utils.concat([swapData, deadline, signature]);
+      const isAuthorised = await registry.isAuthorised(wallet.address, AUGUSTUS_ADDRESS, AUGUSTUS_ADDRESS, data);
       expect(isAuthorised).to.be.true;
     });
 
-    it("Should swap ETH for DAI on augustus directly", async function () {
-      const balanceBefore = await dai.balanceOf(deployer.address);
-      await deployer.sendTransaction(ethToDaiTransaction);
-      const balanceAfter: BigNumber = await dai.balanceOf(deployer.address);
-      expect(balanceAfter.sub(balanceBefore).gt(0)).to.be.true;
-    });
-
-    it("Should swap ETH for DAI on augustus using the forwader", async function () {
-      const signature = await signCalldata(ethToDaiTransaction.data, ags);
-      const balanceBefore = await dai.balanceOf(deployer.address);
-      await forwarder.delegateToParaswap(signature, ethToDaiTransaction.data, { value: ethAmount });
-      const balanceAfter: BigNumber = await dai.balanceOf(deployer.address);
-      expect(balanceAfter.sub(balanceBefore).gt(0)).to.be.true;
-    });
   });
 });
--- test/paraswap-v5/paraswap.service.ts
+++ test/paraswap-v5/paraswap.service.ts
@@ -1,52 +1,19 @@
-import { BigNumber, BytesLike, ethers, Signer } from "ethers";
-import { ContractMethod, ParaSwap } from "paraswap";
+import { ethers } from "hardhat";
+import { BytesLike, Signer } from "ethers";
 
-const paraSwap = new ParaSwap();
+export const paraswapInterface = new ethers.utils.Interface([`
+  function swapOnUniswap(
+      uint256 amountIn,
+      uint256 amountOutMin,
+      address[] calldata path
+  )
+`]);
 
-export const getTransaction = async (srcToken: string, destToken: string, srcAmount: string, userAddress: string) => {
-  const priceRoute = await paraSwap.getRate(
-    srcToken,
-    destToken,
-    srcAmount,
-    undefined,
-    undefined,
-    { includeContractMethods: [ContractMethod.simpleSwap] }
-  );
-
-  if ("message" in priceRoute) {
-    throw new Error(priceRoute.message);
+export const signCalldata = async (data: BytesLike | undefined, signer: Signer) => {
+  if (!data) {
+    throw new Error("No calldata");
   }
-
-  const transaction = await paraSwap.buildTx(
-    srcToken,
-    destToken,
-    srcAmount,
-    BigNumber.from(priceRoute.destAmount).mul("99").div("100").toString(),
-    priceRoute,
-    userAddress,
-    "argent",
-    undefined,
-    undefined,
-    undefined,
-    { ignoreChecks: true }
-  );
-
-  if ("message" in transaction) {
-    throw new Error(`Failed to build transaction: ${transaction.message}`);
-  }
-
-  transaction.value = BigNumber.from(transaction.value).toHexString();
-  // @ts-ignore
-  transaction.gasPrice = BigNumber.from(transaction.gasPrice).toHexString();
-
-  return transaction;
+  const messageHash = ethers.utils.keccak256(data);
+  const messageBytes = ethers.utils.arrayify(messageHash);
+  return await signer.signMessage(messageBytes);
 };
-
-  export const signCalldata = async (data: BytesLike | undefined, signer: Signer) => {
-    if (!data) {
-      throw new Error("No calldata");
-    }
-    const messageHash = ethers.utils.keccak256(data);
-    const messageBytes = ethers.utils.arrayify(messageHash);
-    return await signer.signMessage(messageBytes);
-  };
