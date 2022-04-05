e874f48f79cd0f4253c19c2481b25cf6791a52f5
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-04-05 16:09:36 +0100

    chore(paraswap): add staging deployment

--- scripts/deploy-paraswap-v5.ts
+++ scripts/deploy-paraswap-v5.ts
@@ -14,11 +14,12 @@ export async function main() {
 
   const dappRegistry = await ethers.getContractAt("DappRegistry", config.dappRegistry.address);
   const [deployer] = await ethers.getSigners();
+  console.log("Deployer is", deployer.address);
 
   // Temporarily give ownership of DappRegistry to deployment account if needed
   const registryOwner = await dappRegistry.registryOwners(TRUSTLIST);
   if (registryOwner != deployer.address) {
-    const multisigExecutor = new MultisigExecutor();
+    const multisigExecutor = new MultisigExecutor(config.argent.multisig.autosign);
     await multisigExecutor.connect(registryOwner);
     await multisigExecutor.executeCall(dappRegistry, "changeOwner", [TRUSTLIST, deployer.address]);
   }
@@ -26,6 +27,7 @@ export async function main() {
   // Add filters
   const Filter = await ethers.getContractFactory("ParaswapV5Filter");
   const filter = await Filter.deploy(config.masterSigner.address);
+
   await dappRegistry.addDapp(TRUSTLIST, config.masterSigner.address, filter.address);
   configUpdate.paraswapV5.filter = filter.address;
   console.log(`Added Paraswap v5 filter ${filter.address} with master signer ${config.masterSigner.address}`);
